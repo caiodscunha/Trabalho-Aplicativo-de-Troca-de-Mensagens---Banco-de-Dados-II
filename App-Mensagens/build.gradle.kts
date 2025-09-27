0e20ef4604897d9d33b703f06fd1412626089565
Caio Cunha <160679697+caiodscunha@users.noreply.github.com>
2025-09-27 13:35:58 -0300

    chore: added mongoDB dependencies

--- App-Mensagens/build.gradle.kts
+++ App-Mensagens/build.gradle.kts
@@ -10,6 +10,8 @@ repositories {
 }
 
 dependencies {
+    implementation("org.mongodb:mongodb-driver-sync")
+    implementation(platform("org.mongodb:mongodb-driver-bom:5.6.0"))
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
--- App-Mensagens/src/main/java/br/edu/puc/Main.java
+++ App-Mensagens/src/main/java/br/edu/puc/Main.java
@@ -3,6 +3,5 @@ package br.edu.puc;
 public class Main {
     public static void main(String[] args) {
 
-
     }
 }
