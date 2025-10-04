be6103faaea6e0c8e3f8493e08f33ccaad32f57f
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-10-04 17:38:42 -0300

    feat: Adicionando fetchByReceiver() e método de documentToMessage()
    Adicionando o método fetchByReceiver() da classe Message, usando o MongoHandler findDocuments()

--- App-Mensagens/src/main/java/models/Message.java
+++ App-Mensagens/src/main/java/models/Message.java
@@ -2,12 +2,16 @@ package models;
 
 import jdk.jshell.spi.ExecutionControl;
 
+import java.io.ObjectStreamException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import org.bson.Document;
 
 
 public class Message {
+    private static final String COLLECTION_NAME = "messages";
+
     private LocalDateTime sendedAt;
     private String encryptedMessage;
     private String senderName;
@@ -34,6 +38,12 @@ public class Message {
                 .append("sendedAt", sendedAt);
     }
 
+    public static Message documentToMessage(Document d) {
+        if (d == null) return null;
+
+        return new Message(d.getString("senderName"), d.getString("receiverName"), d.getString("encryptedMessage"), (LocalDateTime) d.get( "sendetAt"));
+    }
+
     /*salva nova mensagem no Mongo, utilizando a classe static MongoUtil (a ser implementada)
 
      */
@@ -45,8 +55,19 @@ public class Message {
     /*carrega as mensagem no Mongo, utilizando a classe static MongoUtil (a ser implementada)
 
      */
-    public static List<Message> fetchByReceiver(String receiver) throws ExecutionControl.NotImplementedException{
-        throw new ExecutionControl.NotImplementedException("");
+    public static List<Message> fetchByReceiver(String receiver) {
+        Document query = new Document("receiverName", receiver);
+
+        var messagesDocuments = MongoHandler.findDocuments(COLLECTION_NAME, query);
+        List<Message> messages = new ArrayList<>();
+
+        for (Document doc : messagesDocuments) {
+            Message message = documentToMessage(doc);
+            if (message != null) {
+                messages.add(message);
+            }
+        }
+        return messages;
     }
 
     public String getDecryptedMessage(String key) throws ExecutionControl.NotImplementedException {
