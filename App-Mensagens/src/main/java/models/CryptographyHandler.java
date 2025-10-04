ecc200d5d371103a59d984671781f01d6360fed3
Caio Cunha <160679697+caiodscunha@users.noreply.github.com>
2025-10-04 19:26:37 -0300

    chore: created CryptographyHandler Structure

---
+++ App-Mensagens/src/main/java/models/CryptographyHandler.java
@@ -0,0 +1,13 @@
+package models;
+
+import jdk.jshell.spi.ExecutionControl;
+
+public class CryptographyHandler {
+    public static String encrypt(String key, String message){
+        throw  new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public static String decrypt(String key, String message){
+        throw  new UnsupportedOperationException("Not supported yet.");
+    }
+}
