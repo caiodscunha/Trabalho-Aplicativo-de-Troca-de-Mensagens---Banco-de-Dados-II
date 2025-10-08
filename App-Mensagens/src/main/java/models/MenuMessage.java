3989b5b1772162d41eccac3ceeb2aacb74ca6030
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-10-08 08:36:57 -0300

    feat: Adicionando método para salvar mensagem

--- App-Mensagens/src/main/java/models/MenuMessage.java
+++ App-Mensagens/src/main/java/models/MenuMessage.java
@@ -5,16 +5,14 @@ import java.util.Scanner;
 
 public class MenuMessage {
     private final Scanner scanner = new Scanner(System.in);
-    private final String receiverName;
+    private final String senderName;
     private final MessageRepository messageRepository = new MessageRepository();
 
-    public MenuMessage(String receiverName) {
-        this.receiverName = receiverName;
+    public MenuMessage(String senderName) {
+        this.senderName = senderName;
     }
 
-
-    public void chooseOption()
-    {
+    public void chooseOption() throws Exception {
         int option = 0;
 
         while (option != 3)
@@ -48,14 +46,32 @@ public class MenuMessage {
 
     public void seeMessages()
     {
-        MessageRepository.loadMessagesForReceiver(receiverName);
+        MessageRepository.loadMessagesForReceiver(senderName);
         messageRepository.printSummary();
         System.out.println("Aperte ENTER para voltar para o menu!");
         scanner.nextLine();
     }
 
-    public void sendMessage()
-    {
+    public void sendMessage() throws Exception {
+        System.out.println("para: ");
+        String receiverName = scanner.next();
+
+        System.out.println("Mensagem: ");
+        String message = scanner.next();
+
+        System.out.println("Chave: ");
+        String chave = scanner.next();
+        try {
+            Message msg = new Message(this.senderName, receiverName, message, chave);
+            msg.saveNewMessage();
+        }
+        catch (Exception e) {
+            System.out.println("Erro ao enviar mensagem");
+        }
+
+
+
+
 
     }
 
